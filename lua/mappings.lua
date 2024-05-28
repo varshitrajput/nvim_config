2afb230c2b5a6881f39634b178d89f6d3ea90643
varshitrajput <rajputvarshit@gmail.com>
2024-05-28 14:08:59 +0530

    added lua

---
+++ lua/mappings.lua
@@ -0,0 +1,15 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+   {
+     "neovim/nvim-lspconfig",
+     config = function()
+       require("nvchad.configs.lspconfig").defaults()
+       require "configs.lspconfig"
+     end,
+   },
+  {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+  },
+  
+  {"mfussenegger/nvim-jdtls"},
+   {
+   	"williamboman/mason.nvim",
+   	opts = {
+   		ensure_installed = {
+   			"lua-language-server", "stylua",
+   			"html-lsp", "css-lsp" , "prettier"
+   		},
+   	},
+   },
+   {
+   	"nvim-treesitter/nvim-treesitter",
+   	opts = {
+   		ensure_installed = {
+   			"vim", "lua", "vimdoc",
+        "html", "css", "java"
+   		},
+   	},
+   },
+  {
+  'nvim-java/nvim-java',
+  dependencies = {
+    'nvim-java/lua-async-await',
+    'nvim-java/nvim-java-refactor',
+    'nvim-java/nvim-java-core',
+    'nvim-java/nvim-java-test',
+    'nvim-java/nvim-java-dap',
+    'MunifTanjim/nui.nvim',
+    'neovim/nvim-lspconfig',
+    'mfussenegger/nvim-dap',
+    {
+      'williamboman/mason.nvim',
+      opts = {
+        registries = {
+          'github:nvim-java/mason-registry',
+          'github:mason-org/mason-registry',
+        },
+      },
+    }
+  },
+}
+}
